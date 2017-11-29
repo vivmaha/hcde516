662f0bf69b7e0f9f2e2d6f8c0911fe0e367878bc
Vivek Maharajh <vivmaha@live.com>
2017-11-28 21:19:55 -0800

    finished final

--- actual/process.R
+++ actual/process.R
@@ -1,6 +1,9 @@
 script.dir <- dirname(sys.frame(1)$ofile)
 
-# Get approval survey codes from m turk data
+#################################################################
+# Get approval survey codes from mTurk data
+#
+
 data.mturk.file <- file.path(script.dir, "data-mturk.csv")
 M <- read.csv(data.mturk.file, stringsAsFactors = FALSE)
 cat(paste("Total entries:", length(M$AssignmentStatus), "\n"))
@@ -12,22 +15,30 @@ cat(paste("Approved entries:", sum(M$AssignmentStatus == "Approved") - 1, "\n"))
 approvedCodes <- M[M$AssignmentStatus == "Approved", "Answer.surveycode"]
 approvedCodes <- c(approvedCodes, "CompletionCode")
 
-read.data.qualtrics <- function(x) {
+#################################################################
+# Get response data from qualtrics data
+#
+
+read.data.qualtrics <- function(filename, fontname) {
   # Get survey result data from qualtrics (and filter to the ones approved in m turk)
-  data.qualtrics.file <- file.path(script.dir, x)
+  data.qualtrics.file <- file.path(script.dir, filename)
   D <- read.csv(data.qualtrics.file , stringsAsFactors = FALSE)
+  # Remove the first row, which is a header
+  D <- tail(D,-1)
   D <- D[D$CompletionCode %in% approvedCodes,]
+  D$Font <- fontname
+  names(D)[names(D) == 'D1'] <- 'YearOfBirth'
+  names(D)[names(D) == 'D2'] <- 'Sex'
+  D
 }
 
-D.serif <- read.data.qualtrics("data-serif-qualtrics.csv")
-D.sans <- read.data.qualtrics("data-sans-qualtrics.csv")
+D.serif <- read.data.qualtrics("data-serif-qualtrics.csv", "serif")
+D.sans <- read.data.qualtrics("data-sans-qualtrics.csv", "sans-serif")
 D <- rbind(D.serif, D.sans)
 
-questions.science = list(D$S01, D$S02, D$S03, D$S04, D$S05, D$S06, D$S07, D$S08, D$S09, D$S10, D$S11, D$S12, D$S13, D$S14, D$S15, D$S16, D$S17, D$S18, D$S19, D$S20)
-questions.pop = list(D$P01, D$Q82, D$Q83, D$Q84, D$Q85, D$Q87, D$Q88, D$Q89, D$Q90, D$Q91, D$Q92, D$Q93, D$Q94, D$Q95, D$Q96, D$Q97, D$Q98, D$Q99, D$Q100)
-# NOTE: One of the pop culture questions was blank by mistake (D$Q86) so data for this question was discarded.
-
-stop()
+#################################################################
+# Convert likert strings to numerical values and average them per-participant to create an "Agreement" column
+#
 
 # Convert the names on the likert scale (eg. "Strongly Disagree") to numbers (eg. 1)
 likert.to.numerical <- function (x) {
@@ -36,52 +47,20 @@ likert.to.numerical <- function (x) {
   values[x]
 }
 
-
-# Determines if a question should be rejected
-verify.question <- function(agreementData, categoryData) {
+for (row.index in 1 : dim(D)[1]) {
+  row <- D[row.index,]
+  responses.likert <- switch(row$Topic,
+         Science = list(row$S01, row$S02, row$S03, row$S04, row$S05, row$S06, row$S07, row$S08, row$S09, row$S10, row$S11, row$S12, row$S13, row$S14, row$S15, row$S16, row$S17, row$S18, row$S19, row$S20),
+         # NOTE: One of the pop culture questions was blank by mistake (D$Q86) so data for this question was discarded.
+         PopCulture = list(row$P01, row$Q82, row$Q83, row$Q84, row$Q85, row$Q87, row$Q88, row$Q89, row$Q90, row$Q91, row$Q92, row$Q93, row$Q94, row$Q95, row$Q96, row$Q97, row$Q98, row$Q99, row$Q100)
+  )
+  responses.numerical = sapply(responses.likert, likert.to.numerical)
   
-  question <- substr(agreementData[1],nchar("I believe the following statement:\n") + 1, 10000)
-  question <- trimws(gsub("[\r\n]", "", question))
-  
-  cat(paste("Q.", question, "\n"))
-  
-  # The first to rows are header information, not real sample points from participants
-  agreementData <- tail(agreementData, -1)
-  categoryData <- tail(categoryData, -1)
-  
-  agreementData <- likert.to.numerical(agreementData)
-  
-  verify.threshold.check <- function(threshold, predicate) {
-    sum(predicate) > threshold * length(predicate)
-  }
-  
-  verify.reject.message <- function(threshold, message) {
-    cat(paste("! REJECT the above question because over", threshold, "of participants", message, "\n"))
-  }
-  
-  verify <- function(threshold, predicate, message) {
-    if (verify.threshold.check(threshold, predicate)) {
-      verify.reject.message(threshold, message)
-    }
-  }
-  
-  # Reject questions where 75% of folks answered "strongly disagree", "undecided", or "strongly agree"
-  extremeValues <- c(1,3,5)
-  for (extremeValue in extremeValues) {
-    verify(0.75, agreementData == extremeValue, paste("responded with a value of", extremeValue))
-  }
-  
-  # Reject questions where 90% of folks agreed, or 90% of folks disagreed
-  verify(0.9, agreementData > 3, "agreed")
-  verify(0.9, agreementData < 3, "disagreed")
-  
-  # Reject questions unless over 80% of folks can agree on a category (science vs. pop culture)
-  if (!verify.threshold.check(0.8, categoryData == "Science") &&
-      !verify.threshold.check(0.8, categoryData == "Pop Culture")) {
-    verify.reject.message(0.8, "did not agree on a category (science vs. pop culture)")
-  }
+  D$Agreement[row.index] = mean(responses.numerical)
 }
 
-for (i in 1 : length(agreementQuestions)) {
-  verify.question(agreementQuestions[[i]], categoryQuestions[[i]])
-}
+#################################################################
+# Get rid of all the columns we don't care about
+#
+
+D <- D[,c("YearOfBirth", "Sex", "Topic", "Font", "Agreement")]
