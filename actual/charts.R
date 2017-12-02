16fde3039af78b09a0fa0361ee5ee4c648d67621
Vivek Maharajh <vivmaha@live.com>
2017-12-01 21:11:26 -0800

    done with initial set of charts

--- actual/charts.R
+++ actual/charts.R
@@ -4,26 +4,63 @@
 
 script.dir <- dirname(sys.frame(1)$ofile)
 source(file.path(script.dir, "process.R"))
+library(ggplot2)
 
 D.pop <- D[D$Topic == "PopCulture",]
 D.science <- D[D$Topic == "Science",]
 D$Group <- paste(D$Topic, " & ", D$Font)
 
+start.image <- function(name) {
+  png(file.path(script.dir, "results", "charts", name))
+}
+end.image <- function() {
+  dev.off()
+}
 
-#################################################################
-# Sample size across each group
-#
 
+start.image("Participants - Sample size.png")
 plot <- ggplot(D, aes(Group))
 plot <- plot + geom_bar()
 print(plot)
+end.image()
 
-
-#################################################################
-# Age across all groups
-#
-
+start.image("Participants - Age.png")
 plot <- ggplot(D, aes(Group, Age))
 plot <- plot + geom_boxplot()
 print(plot)
+end.image()
+
+start.image("Participants - Sex.png")
+plot <- ggplot(D, aes(Group))
+plot <- plot + geom_bar(aes(fill = Sex))
+plot <- plot + theme(legend.position = "bottom")
+print(plot)
+end.image()
+
+plot.best.normal.fit <- function(data, name) {
+  start.image(paste("Analysis -", name, "- Best normal fit.png"))
+  plot <- ggplot(data, aes(x=Agreement))
+  plot <- plot + geom_histogram(bins=15, aes(y=..density..))
+  plot <- plot + stat_function(fun=dnorm, args=list(
+    mean=mean(data$Agreement, na.rm = TRUE),
+    sd=sd(data$Agreement, na.rm = TRUE)))
+  print(plot)
+  end.image()  
+}
+
+plot.best.normal.fit(D.pop, "Pop culture")
+plot.best.normal.fit(D.science, "Science")
+
+plot.comparison <- function(data, name) {
+  start.image(paste("Analysis -", name, "- Comparison of means.png"))
+  plot <- ggplot(data, aes(Font, Agreement))
+  plot <- plot + geom_boxplot()
+  print(plot)
+  end.image()  
+}
+
+plot.comparison(D.pop, "Pop culture")
+plot.comparison(D.science, "Science")
+
+
 
